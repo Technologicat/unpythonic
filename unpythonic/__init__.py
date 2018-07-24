f01eb402d3d9962d7cb05be2a2252da4a4559fb8
Juha Jeronen <juha.jeronen@jyu.fi>
2018-07-24 12:55:20 +0300

    default to pythonic let

--- unpythonic/__init__.py
+++ unpythonic/__init__.py
@@ -7,6 +7,6 @@ See dir(unpythonic) and submodule docstrings for more info.
 
 from .assignonce import *
 from .dynscope import *
-from .let_lispy import *  # guaranteed evaluation order, clunky syntax
-#from .let_pythonic import *  # no guarantees on evaluation order, nice syntax
+#from .lispy_let import *  # guaranteed evaluation order, clunky syntax
+from .pythonic_let import *  # no guarantees on evaluation order, nice syntax
 from .misc import *
