c1be269cb238e8ec7fcb5614f3e9f80e1bfeb29d
Juha Jeronen <juha.jeronen@jyu.fi>
2018-07-24 12:55:09 +0300

    load assignonce in __init__

--- unpythonic/__init__.py
+++ unpythonic/__init__.py
@@ -5,8 +5,8 @@
 See dir(unpythonic) and submodule docstrings for more info.
 """
 
+from .assignonce import *
 from .dynscope import *
 from .let_lispy import *  # guaranteed evaluation order, clunky syntax
 #from .let_pythonic import *  # no guarantees on evaluation order, nice syntax
 from .misc import *
-from .norebind import *
