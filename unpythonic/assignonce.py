40d2063f10235aa3aa2c72c67fbd683210429299
Juha Jeronen <juha.jeronen@jyu.fi>
2018-07-25 15:31:10 +0300

    refactor assignonce to use letenv

--- unpythonic/assignonce.py
+++ unpythonic/assignonce.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Assign-once names."""
+"""Assign-once environment."""
 
 __all__ = ["assignonce"]
 
-# To be able to create _env, in __setattr__ we must special-case it,
-# falling back to default behaviour (which is object.__setattr__,
-# hence super().__setattr__).
-#
-# __getattr__ is never called if standard attribute lookup succeeds,
-# so there we don't need a hook for _env (as long as we don't try to
-# look up _env before it is created).
-#
-# __enter__ should "return self" to support the binding form "with ... as ...".
-#
-# https://docs.python.org/3/reference/datamodel.html#object.__setattr__
-# https://docs.python.org/3/reference/datamodel.html#object.__getattr__
+from unpythonic.letenv import env as _envcls
 
-class assignonce:
+class assignonce(_envcls):
     """Environment with assign-once names.
 
     In Scheme terms, this makes ``define`` and ``set!`` look different::
@@ -26,89 +15,29 @@ class assignonce:
             e.foo = "bar"            # new definition, ok
             e.set("foo", "tavern")   # explicitly rebind e.foo, ok
             e.foo = "quux"           # AttributeError, e.foo already defined.
+
+    If you don't need the automatic clear on exiting the `with` block::
+
+        e = assignonce()
+        e.foo = "bar"
+        e.set("foo", "tavern")
+        e.foo = "quux"  # AttributeError
+
     """
-    def __init__(self):
-        self._env = {}      # should be private...
-
     def __setattr__(self, name, value):
-        if name == "_env":  # ...but this looks clearer with no name mangling.
+        if name == "_env" or name not in self:
             return super().__setattr__(name, value)
-
-        env = self._env
-        if name not in env:
-#            env[name] = self._wrap(name, value)  # with "e.x << newval" rebind syntax.
-            env[name] = value  # no "e.x << newval" rebind syntax.
         else:
             raise AttributeError("name '{:s}' is already defined".format(name))
 
-    def __getattr__(self, name):
-        env = self._env
-        if name in env:
-            return env[name]
-        else:
-            raise AttributeError("name '{:s}' is not defined".format(name))
-
-    def set(self, name, newval):
+    def set(self, name, value):
         """Rebind an existing name to a new value."""
         env = self._env
         if name not in env:
             raise AttributeError("name '{:s}' is not defined".format(name))
-#        env[name] = self._wrap(name, newval)  # with "e.x << newval" rebind syntax.
-        env[name] = newval  # no "e.x << newval" rebind syntax.
-        return newval
-
-    def __lshift__(self, arg):
-        """Alternative syntax for rebinding.
-
-        ``e << ("x", 42)`` is otherwise the same as ``e.set("x", 42)``, except
-        it returns `self`, so that it can be chained::
-
-            e << ("x", 42) << ("y", 23)
-        """
-        name, newval = arg
-        self.set(name, newval)
-        return self
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exctype, excvalue, traceback):
-        pass
-
-    # For rebind syntax: "e.foo << newval" --> "e.foo.__lshift__(newval)",
-    # so foo.__lshift__() must be set up to rebind e.foo.
-    #
-    # For some types (such as int), __lshift__ is read-only.
-    # Also, __ilshift__ does not exist and new attributes cannot be added.
-    # Hence we wrap obj, just adding (or overriding) __lshift__.
-    #
-    # The first call to _wrap(), from setattr(), tells foo its name in e,
-    # capturing it (as well as a reference to e) in the closure of
-    # _assignonce_wrapper. Any re-wrapping (triggered by <<) then
-    # just passes on the same name and e.
-    #
-    # We use << instead of <<= for consistency with let's env, because
-    # there rebind needs to be an expression.
-    #
-    # TODO: doesn't work if obj is a function (not an acceptable base type).
-    #   - Could set up a proxy object providing __lshift__(), and make its
-    #     __call__() call the original function. (Also @functools.wraps it
-    #     to preserve docstring etc.)
-    #   - Then unwrap() needs to know which kind of wrapper it is unwrapping.
-    #   - There may also be other pitfalls beside functions?
-#    def _wrap(self, name, obj):
-#        e = self
-#        class _assignonce_wrapper(obj.__class__):  # new type each time we are called!
-#            def __lshift__(self, newval):
-#                rewrapped = e._wrap(name, unwrap(newval))  # avoid wrapper stacking.
-#                e._env[name] = rewrapped  # bypass setattr() so that it can always refuse updates.
-#                return rewrapped
-#        def unwrap(obj):  # find first parent class that is not a _wrapper
-#            for cls in obj.__class__.__mro__:
-#                if cls.__name__ != "_assignonce_wrapper":
-#                    return cls(obj)  # copy-construct obj without wrapper
-#            assert False, "wrapped value missing in {} {}".format(type(obj), obj)
-#        return _assignonce_wrapper(obj)  # copy-construct obj with wrapper
+        # important part: bypass our own __setattr__, which would refuse the update.
+        super().__setattr__(name, value)
+        return value
 
 def test():
     with assignonce() as e:
--- unpythonic/letenv.py
+++ unpythonic/letenv.py
@@ -5,23 +5,42 @@
 __all__ = ["env"]
 
 class env:
-    """Bunch with iterator and subscripting support.
+    """Environment for let-like constructs.
 
-    Iteration and subscripting just expose the underlying dict.
+    Essentially a fancy bunch.
 
-    Also works as a bare bunch.
+    Bare bunch::
 
-    Usage:
-        # bare bunch:
-        myenv2 = env(s="hello", orange="fruit", answer=42)
-        print(myenv2.s)
-        print(myenv2)
+        e = env(s="hello", orange="fruit", answer=42)
+        print(e.s)
 
-        # iteration and subscripting:
-        names = [k for k in myenv2]
+    Printing support for debugging::
 
-        for k,v in myenv2.items():
-            print("Name {} has value {}".format(k, v))
+        print(e)
+
+    Iteration and subscripting::
+
+        names = [name for name in e]
+
+        data = [(name, e[name]) for name in e]
+
+        for k, v in e.items():
+            print("name {} has value {}".format(k, v))
+
+    Membership testing::
+
+        if "answer" in e:
+            print("e has an answer")
+        else:
+            print("e has no answer")
+
+    Context manager::
+
+        with env(s="hello", orange="fruit", answer=42) as e2:
+            ...  # ...code that uses e2...
+
+    When the `with` block exits, `e2` forgets all its bindings. The `e2`
+    instance itself will remain alive due to Python's scoping rules.
     """
     def __init__(self, **bindings):
         self._env = {}
@@ -29,10 +48,12 @@ class env:
             self._env[name] = value
 
     # item access by name
-    #
+    # https://docs.python.org/3/reference/datamodel.html#object.__setattr__
+    # https://docs.python.org/3/reference/datamodel.html#object.__getattr__
     def __setattr__(self, name, value):
         if name == "_env":  # hook to allow creating _env directly in self
             return super().__setattr__(name, value)
+#        value = self._wrap(name, value)  # for "e.x << value" rebind syntax.
         self._env[name] = value  # make all other attrs else live inside _env
 
     def __getattr__(self, name):
@@ -40,10 +61,13 @@ class env:
         if name in env:
             return env[name]
         else:
-            raise AttributeError("Name '{:s}' not in environment".format(name))
+            raise AttributeError("name '{:s}' is not defined".format(name))
+
+    # membership test (in, not in)
+    def __contains__(self, k):
+        return self._env.__contains__(k)
 
     # iteration
-    #
     def __iter__(self):
         return self._env.__iter__()
 
@@ -54,21 +78,25 @@ class env:
         return self._env.items()
 
     # subscripting
-    #
     def __getitem__(self, k):
         return getattr(self, k)
 
     def __setitem__(self, k, v):
         setattr(self, k, v)
 
+    # context manager
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exctype, excvalue, traceback):
+        self.clear()
+
     # pretty-printing
-    #
     def __str__(self):
-        bindings = ["{}: {}".format(name,value) for name,value in self._env.items()]
-        return "<env: <{:s}>>".format(", ".join(bindings))
+        bindings = ["{:s}={}".format(name,repr(value)) for name,value in self._env.items()]
+        return "<env object at 0x{:x}: {{{:s}}}>".format(id(self), ", ".join(bindings))
 
     # other
-    #
     def set(self, name, value):
         """Convenience method to allow assignment in expression contexts.
 
@@ -76,7 +104,7 @@ class env:
 
         For convenience, returns the `value` argument.
         """
-        if not hasattr(self, name):  # allow only rebinding
+        if name not in self:  # allow only rebinding
             raise AttributeError("name '{:s}' is not defined".format(name))
         setattr(self, name, value)
         return value  # for convenience
@@ -92,3 +120,42 @@ class env:
         name, value = arg
         self.set(name, value)
         return self
+
+    def clear(self):
+        """Clear the environment, i.e. forget all bindings."""
+        self._env = {}
+
+    # For rebind syntax: "e.foo << newval" --> "e.foo.__lshift__(newval)",
+    # so foo.__lshift__() must be set up to rebind e.foo.
+    #
+    # For some types (such as int), __lshift__ is read-only.
+    # Also, __ilshift__ does not exist and new attributes cannot be added.
+    # Hence we wrap obj, just adding (or overriding) __lshift__.
+    #
+    # The first call to _wrap(), from setattr(), tells foo its name in e,
+    # capturing it (as well as a reference to e) in the closure of
+    # _assignonce_wrapper. Any re-wrapping (triggered by <<) then
+    # just passes on the same name and e.
+    #
+    # We use << instead of <<= for consistency with let's env, because
+    # there rebind needs to be an expression.
+    #
+    # TODO: doesn't work if obj is a function (not an acceptable base type).
+    #   - Could set up a proxy object providing __lshift__(), and make its
+    #     __call__() call the original function. (Also @functools.wraps it
+    #     to preserve docstring etc.)
+    #   - Then unwrap() needs to know which kind of wrapper it is unwrapping.
+    #   - There may also be other pitfalls beside functions?
+#    def _wrap(self, name, obj):
+#        e = self
+#        class _assignonce_wrapper(obj.__class__):  # new type each time we are called!
+#            def __lshift__(self, newval):
+#                rewrapped = e._wrap(name, unwrap(newval))  # avoid wrapper stacking.
+#                e._env[name] = rewrapped  # bypass setattr() so that it can always refuse updates.
+#                return rewrapped
+#        def unwrap(obj):  # find first parent class that is not a _wrapper
+#            for cls in obj.__class__.__mro__:
+#                if cls.__name__ != "_assignonce_wrapper":
+#                    return cls(obj)  # copy-construct obj without wrapper
+#            assert False, "wrapped value missing in {} {}".format(type(obj), obj)
+#        return _assignonce_wrapper(obj)  # copy-construct obj with wrapper
