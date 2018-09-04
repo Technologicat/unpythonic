bc96e23fdcb03441bcb35442b8448aa27f8837f0
Juha Jeronen <juha.jeronen@jyu.fi>
2018-09-04 05:58:59 +0300

    oops, add missing file

---
+++ unpythonic/rc.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Internal configuration for unpythonic."""
+
+# TCO implementation. Available: "exc", "fast".
+# See enable_fasttco() in __init__.py.
+_tco_impl = "exc"
