82b2c934bb68b0b922dceb481c47e4817742b42b
Juha Jeronen <juha.jeronen@jyu.fi>
2018-10-04 17:06:14 +0300

    change macro example main to unit test format

--- macro_extras/main.py
+++ macro_extras/main.py
@@ -17,39 +17,43 @@ from unpythonic.syntax import macros, \
                               prefix, q, u, kw, \
                               λ
 
-from unpythonic import foldr, composerc as compose, cons, nil, apply
+from unpythonic import foldr, composerc as compose, cons, nil, ll, apply
 
 def main():
     with curry:
         mymap = lambda f: foldr(compose(cons, f), nil)
         double = lambda x: 2 * x
-        print(mymap(double, (1, 2, 3)))
+        assert mymap(double, (1, 2, 3)) == ll(2, 4, 6)
 
         def add3(a, b, c):
             return a + b + c
-        print(add3(1)(2)(3))
-        print(add3(1, 2)(3))
-        print(add3(1)(2, 3))
-        print(add3(1, 2, 3))
+        assert add3(1)(2)(3) == 6
+        assert add3(1, 2)(3) == 6
+        assert add3(1)(2, 3) == 6
+        assert add3(1, 2, 3) == 6
 
         # NOTE: because builtins cannot be inspected, curry just no-ops on them.
         # So this won't work:
         from operator import add
         try:
             f = add(1)
-            print(f(2))
+            assert f(2) == 3
         except TypeError:
             pass
+        else:
+            assert False, "update documentation"
         # In cases like this, make a wrapper:
         myadd = lambda a, b: add(a, b)
         f = myadd(1)
-        print(f(2))
+        assert f(2) == 3
 
     # outside the with block, autocurry is not active, so this is an error:
     try:
         add3(1)
     except TypeError:
         pass
+    else:
+        assert False
 
     # Write Python almost like Lisp!
     with prefix:
@@ -61,114 +65,139 @@ def main():
         (print, t1, t2)
         # unquote operator u locally turns the transformation back on:
         t3 = (q, (u, print, 42), (print, 42), "foo", "bar")
-        (print, t3)
+        assert t3 == (q, None, (print, 42), "foo", "bar")
+
         # quotes nest; call transformation made when quote level == 0
         t4 = (q, (print, 42), (q, (u, u, print, 42)), "foo", "bar")
-        (print, t4)
+        assert t4 == (q, (print, 42), (None,), "foo", "bar")
+
         # Be careful:
         try:
             (x,)  # in a prefix block, this means "call the 0-arg function x"
         except TypeError:
             pass  # 'int' object is not callable
+        else:
+            assert False, "should have attempted to call x"
         (q, x)  # OK!
 
         # give named args with kw(...) [it's syntax, not really a function!]:
         def f(*, a, b):
-            (print, a, b)
-        (f, kw(a="hi there", b="foo"))      # in one kw(...), or...
-        (f, kw(a="hi there"), kw(b="foo"))  # in several kw(...), doesn't matter
-        (f, kw(a="hi there"), kw(b="foo"), kw(b="bar"))  # rightmost duplicate wins
+            return (q, a, b)
+        # in one kw(...), or...
+        assert (f, kw(a="hi there", b="foo")) == (q, "hi there", "foo")
+        # in several kw(...), doesn't matter
+        assert (f, kw(a="hi there"), kw(b="foo")) == (q, "hi there", "foo")
+        # in case of duplicate name across kws, rightmost wins
+        assert (f, kw(a="hi there"), kw(b="foo"), kw(b="bar")) == (q, "hi there", "bar")
 
         # give *args with unpythonic.fun.apply, like in Lisps:
         lst = [1, 2, 3]
         def g(*args):
-            print(args)
-        (apply, g, lst)
-        (apply, g, "hi", "ho", lst)  # lst goes last; may have other args first
+            return args
+        assert (apply, g, lst) == (q, 1, 2, 3)
+        # lst goes last; may have other args first
+        assert (apply, g, "hi", "ho", lst) == (q, "hi" ,"ho", 1, 2, 3)
 
     # Introducing LisThEll:
     with prefix, curry:  # important: apply prefix first, then curry
         mymap = lambda f: (foldr, (compose, cons, f), nil)
         double = lambda x: 2 * x
         (print, (mymap, double, (q, 1, 2, 3)))
+        assert (mymap, double, (q, 1, 2, 3)) == ll(2, 4, 6)
 
     # Let macros. Lexical scoping supported.
     #
-    let((x, 17),  # parallel binding, i.e. bindings don't see each other
-        (y, 23))[
-          print(x, y)]
+    assert let((x, 17),  # parallel binding, i.e. bindings don't see each other
+               (y, 23))[
+                 (x, y)] == (17, 23)
 
-    letseq((x, 1),  # sequential binding, i.e. Scheme/Racket let*
-           (y, x+1))[
-             print(x, y)]
+    assert letseq((x, 1),  # sequential binding, i.e. Scheme/Racket let*
+                  (y, x+1))[
+                    (x, y)] == (1, 2)
+
+    try:
+        let((X, 1),
+            (y, X+1))[  # parallel binding, doesn't see the x in the same let
+              print(X, y)]
+    except NameError:
+        pass  # no X in surrounding scope
+    else:
+        assert False, "should not see the X in the same let"
+
+    try:
+        letseq((X, y+1),
+               (y, 2))[
+                 (X, y)]
+    except NameError:  # y is not yet defined on the first line
+        pass
+    else:
+        assert False, "y should not yet be defined on the first line"
 
     try:
         let((x, 1),
-            (y, x+1))[  # parallel binding, doesn't see the x in the same let
-              print(x, y)]
-    except NameError:
-        pass  # no x in surrounding scope
-
-    try:
-        letseq((x, y+1),
-               (y, 2))[
-                 print(x, y)]
-    except NameError:  # y is not yet defined on the first line
-        pass
-
-    try:
-        let((x, 1), (x, 2))[  # error, cannot rebind the same name
+            (x, 2))[  # error, cannot rebind the same name
               print(x)]
     except AttributeError:
         pass
+    else:
+        assert False, "should not be able to rebind the same name x in the same let"
 
 #    simple_let((x, 1), (x, 2))[  # error, cannot rebind the same name
 #          print(x)]
 
-    letseq((x, 1), (x, x+1))[  # but in a letseq it's ok
-          print(x)]
+    assert letseq((x, 1),
+                  (x, x+1))[  # but in a letseq it's ok
+                    x] == 2
 
     # letrec sugars unpythonic.lispylet.letrec, removing the need for quotes
     # and "lambda e: ..." wrappers (these are inserted by the macro):
-    letrec((evenp, lambda x: (x == 0) or oddp(x - 1)),
-           (oddp,  lambda x: (x != 0) and evenp(x - 1)))[
-             print(evenp(42))]
+    assert letrec((evenp, lambda x: (x == 0) or oddp(x - 1)),
+                  (oddp,  lambda x: (x != 0) and evenp(x - 1)))[
+                    evenp(42)] is True
 
     # nested letrecs work, too - each environment is internally named by a gensym
-    # so that outer ones can be seen:
-    letrec((z, 9000))[
-      letrec((evenp, lambda x: (x == 0) or oddp(x - 1)),
-             (oddp,  lambda x: (x != 0) and evenp(x - 1)))[
-               print(evenp(42), z)]]
+    # so that outer ones "show through":
+    assert letrec((z, 9000))[
+             letrec((evenp, lambda x: (x == 0) or oddp(x - 1)),
+                    (oddp,  lambda x: (x != 0) and evenp(x - 1)))[
+                      (evenp(42), z)]] == (True, 9000)
 
     # also letrec supports lexical scoping, since in MacroPy 1.1.0 and later,
     # macros are expanded from inside out (the z in the inner scope expands to
     # the inner environment's z, which makes the outer expansion leave it alone):
     from unpythonic import begin
+    out = []
     letrec((z, 1))[
-      begin(print(z),
+      begin(out.append(z),
             letrec((z, 2))[
-              print(z)])]  # (be careful with the parentheses!)
+              out.append(z)])]  # (be careful with the parentheses!)
+    assert out == [1, 2]
 
     # assignment updates the innermost value by that name:
+    out = []
     letrec((z, 1))[
-      begin(print("outer z is", z),
-            print("changing outer z to", z << 5),  # assignment to env is an expression, returns the new value
+      begin(out.append(z), # outer z
+            # assignment to env is an expression, returns the new value
+            out.append(z << 5),
             letrec((z, 2))[
-              begin(print("inner z is", z),
-                    print("changing inner z to", z << 7))],
-            print("outer z is still", z))]
+              begin(out.append(z), # inner z
+                    out.append(z << 7))],
+            out.append(z))]  # outer z
+    assert out == [1, 5, 2, 7, 5]
+
+    out = []
     letrec((x, 1))[
-      begin(print("x is", x),
+      begin(out.append(x),
             letrec((z, 2))[
-              begin(print("z is", z),
-                    print("changing x to", x << 7))],
-            print("x is now", x))]
+              begin(out.append(z),
+                    out.append(x << 7))],  # x only defined in outer letrec, updates that
+            out.append(x))]
+    assert out == [1, 2, 7, 7]
 
     # this works, too
-    letrec((x, 1),
-           (y, x+2))[
-      print(y)]
+    assert letrec((x, 1),
+                  (y, x+2))[
+                    (x, y)] == (1, 3)
 
     # but this is an error (just like in Racket):
     try:
@@ -177,37 +206,38 @@ def main():
           print(x)]
     except AttributeError:  # y is not defined on the first line (lispylet and env together detect this)
         pass
+    else:
+        assert False, "y should not be yet defined on the first line"
 
     # this is ok, because the y on the RHS of the definition of f
     # is evaluated only when the function is called
-    letrec((f, lambda t: t + y + 1),
-           (y, 2))[
-      print(f(3))]
+    assert letrec((f, lambda t: t + y + 1),
+                  (y, 2))[
+                    f(3)] == 6
 
     a = letrec((x, 1),
-               (y, x+2))[
-                 begin(x << 1337,
+               (y, x+2))[  # y computed now
+                 begin(x << 1337,  # x updated now, no effect on y
                        (x, y))]
-    print(a)
+    assert a == (1337, 3)
 
     a = let((x, 1),
             (y, 2))[
               begin(y << 1337,
                     (x, y))]
-    print(a)
+    assert a == (1, 1337)
 
     a = letseq((x, 1),
                (y, x+1))[
                  begin(x << 1337,
                        (x, y))]
-    print(a)
+    assert a == (1337, 2)
 
     # let over lambda
     count = let((x, 0))[
-              lambda: begin(x << x + 1,
-                            "count is {}".format(x))]
-    print(count())
-    print(count())
+              lambda: x << x + 1]
+    assert count() == 1
+    assert count() == 2
 
     # multilambda: lambda with implicit begin. Usage:
     #   λ(arg0, ...)[body0, ...]
@@ -216,28 +246,38 @@ def main():
     #  - no *args, **kwargs
     count = let((x, 0))[
               λ()[x << x + 1,
-                  "count is {}".format(x)]]
-    print(count())
-    print(count())
+                  x]]
+    assert count() == 1
+    assert count() == 2
 
     echo = λ(x)[print(x), x]
     z = echo("hi there")
     assert z == "hi there"
 
-    myadd = λ(x, y)[print(x, y), x + y]
+    myadd = λ(x, y)[print("myadding", x, y), x + y]
+    assert myadd(2, 3) == 5
+
+    # But this must be done manually. Having λ with implicit do (instead of begin)
+    # would cause the problem that it is then impossible to assign to let variables
+    # defined outside the λ, because the do then captures all assignments
+    # (because macros are expanded from inside out).
+    myadd = lambda x, y: do[print("myadding", x, y),
+                            tmp << x + y,
+                            print("result is", tmp),
+                            tmp]
     assert myadd(2, 3) == 5
 
     # Anaphoric if: aif[test, then, otherwise]
     # Magic identifier "it" refers to the test result.
-    aif[2*21,
-        print("it is {}".format(it)),
-        print("it is False")]
+    assert aif[2*21,
+               "it is {}".format(it),
+               "it is False"] == "it is 42"
 
     # Lispy "cond" - a human-readable multi-branch conditional for lambdas.
     answer = lambda x: cond[x == 2, "two",
                             x == 3, "three",
                             "something else"]
-    print(answer(42))
+    assert answer(42) == "something else"
 
     # macro wrapper for seq.do (stuff imperative code into a lambda)
     #  - Assignment is ``var << value``.
@@ -256,12 +296,12 @@ def main():
            print(x),
            x << 23,
            x]
-    print(y)
+    assert y == 23
 
     y2 = do0[y << 5,  # y << val assigns, then returns val
              print("hi there, y =", y),
              42]  # evaluated but not used, do0 returns the first value
-    assert(y2 == 5)
+    assert y2 == 5
 
     # macro wrapper for amb.forall
     #   - assignment (with List-monadic magic) is ``var << iterable``
