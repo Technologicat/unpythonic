c238a0bdf0bdb89d89bf276210313b9d515557f6
Juha Jeronen <juha.jeronen@jyu.fi>
2018-10-05 13:17:58 +0300

    unit test also macro_extras

--- runtests.sh
+++ runtests.sh
@@ -17,8 +17,20 @@ for MODULE in unpythonic/*.py; do
   fi
 done
 
+echo -ne "\e[32m*** Testing macro_extras (DANGER: against INSTALLED unpythonic) ***\e[39m\n"
+cd macro_extras
+./macropy3 main
+if [ $? -ne 0 ]; then
+  echo -ne "\e[91m*** FAIL in macro_extras ***\e[39m\n"
+  let FAILS+=1
+else
+  echo -ne "\e[92m*** PASS ***\e[39m\n"
+fi
+cd ..
+
 if [ $FAILS -gt 0 ]; then
       echo -ne "\e[91m*** At least one FAIL ***\e[39m\n"
 else
       echo -ne "\e[92m*** ALL OK ***\e[39m\n"
 fi
+
