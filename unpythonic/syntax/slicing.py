2f58a168d422453cdb9e82f49b1acbe456f8699d
Juha Jeronen <juha.jeronen@jyu.fi>
2019-02-26 23:12:50 +0200

    update missed docstring

--- unpythonic/syntax/slicing.py
+++ unpythonic/syntax/slicing.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Functionally update sequences - macro wrapper for slice syntax."""
+"""Macro wrappers for operations using slice syntax."""
 
 from ast import BinOp, LShift, Subscript, Index, Slice, ExtSlice
 
