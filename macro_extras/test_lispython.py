f264bd9d9530d5667db42772c9e16d2fe73bf999
Juha Jeronen <juha.jeronen@jyu.fi>
2019-03-05 17:37:43 +0200

    extend lispython tests

--- macro_extras/test_lispython.py
+++ macro_extras/test_lispython.py
@@ -18,17 +18,28 @@ from operator import mul
 def main():
     print("hello, my dialect is {}".format(__lang__))
 
-    x = let[(a, 21) in 2*a]
-    assert x == 42
+    assert prod((2, 3, 4)) == 24  # bye missing battery, hello new dialect builtin
+    assert foldl(mul, 1, (2, 3, 4)) == 24
 
+    # cons, car, cdr, ll, llist are builtins (for more linked list utils, import from unpythonic)
     c = cons(1, 2)
     assert tuple(c) == (1, 2)
     assert car(c) == 1
     assert cdr(c) == 2
+    assert ll(1, 2, 3) == llist((1, 2, 3))
 
-    assert prod((2, 3, 4)) == 24  # bye missing battery, hello new dialect builtin
+    # all unpythonic.syntax let[], letseq[], letrec[] constructs are builtins
+    # (including the decorator versions, let_syntax and abbrev)
+    x = let[(a, 21) in 2*a]
+    assert x == 42
 
-    # auto-TCO, implicit return in tail position
+    x = letseq[((a, 1),
+                (a, 2*a),
+                (a, 2*a)) in
+               a]
+    assert x == 4
+
+    # auto-TCO (both in defs and lambdas), implicit return in tail position
     def fact(n):
         def f(k, acc):
             if k == 1:
@@ -38,6 +49,17 @@ def main():
         f(n, acc=1)
     assert fact(4) == 24
     fact(5000)  # no crash
+
+    t = letrec[((evenp, lambda x: (x == 0) or oddp(x - 1)),
+                (oddp, lambda x:(x != 0) and evenp(x - 1))) in
+               evenp(10000)]  # no crash
+    assert t is True
+
+#    # TODO: investigate what goes wrong here, looks like a trampoline is missing somewhere?
+#    t = letrec[((evenp, lambda x: (x == 0) or oddp(x - 1)),
+#                (oddp, lambda x:(x != 0) and evenp(x - 1))) in
+#               [evenp(10000),
+#               42]]
 
     # lambdas are named automatically
     square = lambda x: x**2
@@ -49,7 +71,25 @@ def main():
     assert cube(3) == 27
     assert cube.__name__ == "cube"
 
-    assert foldl(mul, 1, (2, 3, 4)) == 24
+    # lambdas can have multiple expressions and local variables
+    #
+    # (if you need to return a list from a lambda, use an extra set of brackets;
+    #  the outermost brackets always enable multiple-expression mode)
+    #
+    test = lambda x: [local[y << 2*x],  # local[name << value] makes a local variable
+                      y + 1]
+    assert test(10) == 21
+
+    # actually the multiple-expression environment is an unpythonic.syntax.do[],
+    # which can be used in any expression position
+    x = do[local[z << 2],
+           3*z]
+    assert x == 6
+
+    # do0[] is the same, but returns the value of the first expression instead of the last one
+    x = do0[local[z << 3],
+            print("hi from do0, z is {}".format(z))]
+    assert x == 3
 
 if __name__ == '__main__':
     main()
