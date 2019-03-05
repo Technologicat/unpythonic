775e227ec21fcfc2e409480588100a5664170390
Juha Jeronen <juha.jeronen@jyu.fi>
2019-03-06 01:07:25 +0200

    update tests

--- macro_extras/test_lispython.py
+++ macro_extras/test_lispython.py
@@ -2,16 +2,16 @@
 """Test the Lispython dialect."""
 
 # The lang-import must be the first statement after the module docstring, if any.
+# It sets the dialect this module is parsed in.
 from __lang__ import lispython
 
 # unpythonic is lispython's stdlib; not everything gets imported by default
 from unpythonic import foldl
 
-# of course, Python's stdlib is available too
+# of course, all of Python's stdlib is available too
 #
-# So is **any** Python library; the ability to use arbitrary Python libraries
-# in a new language with Python-based but customized syntax pretty much being
-# the whole point of Pydialect.
+# So is **any** Python library; the ability to use arbitrary Python libraries in
+# a customized Python-based language is pretty much the whole point of Pydialect.
 #
 from operator import mul
 
@@ -21,7 +21,7 @@ def main():
     assert prod((2, 3, 4)) == 24  # bye missing battery, hello new dialect builtin
     assert foldl(mul, 1, (2, 3, 4)) == 24
 
-    # cons, car, cdr, ll, llist are builtins (for more linked list utils, import from unpythonic)
+    # cons, car, cdr, ll, llist are builtins (for more linked list utils, import them from unpythonic)
     c = cons(1, 2)
     assert tuple(c) == (1, 2)
     assert car(c) == 1
@@ -39,58 +39,74 @@ def main():
                a]
     assert x == 4
 
+    # rackety cond
+    a = lambda x: cond[x < 0, "nope",
+                       x % 2 == 0, "even",
+                       "odd"]
+    assert a(-1) == "nope"
+    assert a(2) == "even"
+    assert a(3) == "odd"
+
     # auto-TCO (both in defs and lambdas), implicit return in tail position
     def fact(n):
         def f(k, acc):
             if k == 1:
-                acc
-            else:  # "else" required to make both branches into tail positions
-                f(k - 1, k*acc)
+                return acc  # "return" still available for early return
+            f(k - 1, k*acc)
         f(n, acc=1)
     assert fact(4) == 24
-    fact(5000)  # no crash
+    fact(5000)  # no crash (and correct result, since Python uses bignums transparently)
 
     t = letrec[((evenp, lambda x: (x == 0) or oddp(x - 1)),
                 (oddp, lambda x:(x != 0) and evenp(x - 1))) in
                evenp(10000)]  # no crash
     assert t is True
 
-#    # TODO: investigate what goes wrong here, looks like a trampoline is missing somewhere?
-#    # TODO: also, seems evenp and oddp don't get __name__ here, although they should (as let-bindings)
-#    t = letrec[((evenp, lambda x: (x == 0) or oddp(x - 1)),
-#                (oddp, lambda x:(x != 0) and evenp(x - 1))) in
-#               [evenp(10000),
-#               42]]
-
     # lambdas are named automatically
     square = lambda x: x**2
     assert square(3) == 9
     assert square.__name__ == "square"
 
-    # underscore (NOTE: due to this, "f" is a reserved name in lispython)
+    # the underscore (NOTE: due to this, "f" is a reserved name in lispython)
     cube = f[_**3]
     assert cube(3) == 27
     assert cube.__name__ == "cube"
 
     # lambdas can have multiple expressions and local variables
     #
-    # (if you need to return a list from a lambda, use an extra set of brackets;
-    #  the outermost brackets always enable multiple-expression mode)
+    # If you need to return a list from a lambda, use an extra set of brackets;
+    # the outermost brackets always enable multiple-expression mode.
     #
     test = lambda x: [local[y << 2*x],  # local[name << value] makes a local variable
                       y + 1]
     assert test(10) == 21
 
+    a = lambda x: [local[t << x % 2],
+                   cond[t == 0, "even",
+                        t == 1, "odd",
+                        None]]  # cond[] requires an else branch
+    assert a(2) == "even"
+    assert a(3) == "odd"
+
     # actually the multiple-expression environment is an unpythonic.syntax.do[],
-    # which can be used in any expression position
+    # which can be used in any expression position.
     x = do[local[z << 2],
            3*z]
     assert x == 6
 
-    # do0[] is the same, but returns the value of the first expression instead of the last one
+    # do0[] is the same, but returns the value of the first expression instead of the last one.
     x = do0[local[z << 3],
             print("hi from do0, z is {}".format(z))]
     assert x == 3
+
+    # MacroPy #21; namedlambda must be in its own with block in the
+    # dialect implementation or this particular combination will fail
+    # (uncaught jump, __name__ not set).
+    t = letrec[((evenp, lambda x: (x == 0) or oddp(x - 1)),
+                (oddp,  lambda x:(x != 0) and evenp(x - 1))) in
+               [local[x << evenp(100)],  # multi-expression let body is a do[] environment
+                (x, evenp.__name__, oddp.__name__)]]
+    assert t == (True, "evenp", "oddp")
 
 if __name__ == '__main__':
     main()
