89df74aa9198e74c5515f7533d0f106e34360084
Juha Jeronen <juha.jeronen@jyu.fi>
2019-03-06 02:58:19 +0200

    update docstring (in preparation of a proper README)

--- macro_extras/lispython.py
+++ macro_extras/lispython.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 """Lispython: the love child of Python and Scheme.
 
-Powered by Pydialect and unpythonic.
-
-This module is the dialect definition, invoked by ``dialects.DialectFinder``
-when it detects a lang-import that matches the module name of this module.
-
-This dialect is implemented in MacroPy.
-
 **Schemers rejoice!**::
 
     Multiple musings mix in a lambda,
@@ -16,6 +9,94 @@ This dialect is implemented in MacroPy.
     Linked lists cons and fold.
     Tail-call into recursion divine,
     The final value always provide.
+
+Powered by Pydialect and unpythonic.
+
+This module is the dialect definition, invoked by ``dialects.DialectFinder``
+when it detects a lang-import that matches the module name of this module.
+
+This dialect is implemented in MacroPy.
+
+**What Lispython is**
+
+The goal of the Lispython dialect is to fix some glaring issues that hamper
+Python when viewed from a Lisp/Scheme perspective. We take that approach of
+a relatively thin layer of macros (and underlying functions that implement
+the actual functionality), minimizing magic as far as reasonably possible.
+
+Performance is only a secondary concern; performance-critical parts fare better
+at the other end of the wide spectrum, with Cython. Lispython is for the
+remaining 80% of the code, where the bottleneck is human developer time.
+
+    https://en.wikipedia.org/wiki/Wide-spectrum_language
+    https://en.wikipedia.org/wiki/Pareto_principle
+    http://cython.org/
+
+The dialect aims at production quality.
+
+**Why extend Python?**
+
+Racket is an excellent Lisp, especially with sweet, sweet expressions, not to
+mention extremely pythonic. The word is *rackety*; the philosophy comes with
+an air of Zen minimalism, but the focus on *batteries included* and
+understandability are remarkably similar to the pythonic ideal. Racket even
+has an IDE and an equivalent of PyPI, and the documentation is simply stellar.
+
+    https://docs.racket-lang.org/sweet/
+    https://sourceforge.net/projects/readable/
+    https://srfi.schemers.org/srfi-110/srfi-110.html
+    https://srfi.schemers.org/srfi-105/srfi-105.html
+    https://racket-lang.org/
+
+Python, on the other hand, has a slight edge in usability to the end-user
+programmer, and importantly, a huge ecosystem of libraries, second to ``None``.
+Python is where science happens (unless you're in CS).
+
+Python is an almost-Lisp that has delivered on the productivity promise of Lisp.
+However, in certain other respects, Python the base language leaves something
+to be desired, if you have been exposed to Racket (or Haskell, but that's a
+different story). Writing macros is harder due to the irregular syntax, but
+thankfully a set of macros only needs to be created once.
+
+    http://paulgraham.com/icad.html
+
+Practicality beats purity: hence, fix the minor annoyances that would otherwise
+quickly add up, and reap the benefits of both worlds. If Python is software
+glue, Lispython is an additive that makes it flow better.
+
+**Note**
+
+For PG's accumulator puzzle even Lispython can do no better than this
+let-over-lambda::
+
+    foo = lambda n0: let[(n, n0) in
+                         (lambda i: n << n + i)]
+    f = foo(10)
+    f(1) # 11
+    f(1) # 12
+
+which still sets up a separate place for the accumulator. The modern pure
+Python solution avoids that, but needs many lines::
+
+    def foo(n):
+        def accumulate(i):
+            nonlocal n
+            n += i
+            return n
+        return accumulate
+    f = foo(10)
+    f(1) # 11
+    f(1) # 12
+
+The problem is that assignment to a lexical variable (including formals) is a
+statement in Python. If we abbreviate ``accumulate`` as a lambda, it needs a
+``let`` environment to write in (to use unpythonic's expression-assignment).
+
+**CAUTION**
+
+No instrumentation exists (or is even planned) for the Lispython layer; you'll
+have to use regular Python tooling to profile, debug, and such. The layer
+should be thin enough for this not to be a major problem in practice.
 """
 
 # TODO: fix the unpythonic.syntax block macros to leave a placeholder for
