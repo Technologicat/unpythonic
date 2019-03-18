b9733a8fdfd39859a336885f173c32e32169a070
Juha Jeronen <juha.jeronen@jyu.fi>
2019-03-18 10:33:06 +0200

    bump version

--- unpythonic/__init__.py
+++ unpythonic/__init__.py
@@ -6,7 +6,7 @@ See ``dir(unpythonic)`` and submodule docstrings for more.
 If you have MacroPy installed, see also ``unpythonic.syntax``.
 """
 
-__version__ = '0.14.0'
+__version__ = '0.14.1'
 
 from .amb import *
 from .arity import *
