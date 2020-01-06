58ca2f259a0788df15ce0d63107bb766d5bf1912
Juha Jeronen <juha.jeronen@jyu.fi>
2020-01-06 04:53:06 +0200

    Improve docstring

--- unpythonic/net/__init__.py
+++ unpythonic/net/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8; -*-
 """Networking and REPL-related utilities.
 
-Symbols recommended for library use are imported directly to this namespace.
+From the submodules, symbols most likely useful for developing applications
+or higher-level libraries are imported directly into this namespace.
 
 The hot-patching REPL implementation is contained in the submodules `server`,
 `client` and `common`; if you need any of that, feel free to import it
