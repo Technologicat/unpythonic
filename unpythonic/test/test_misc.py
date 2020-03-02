23a6b4fcb59cb0b743385e6745f4c9703432ba4d
Juha Jeronen <juha.jeronen@jyu.fi>
2020-03-02 17:35:46 +0200

    Revert "Add `Singleton` metaclass"
    This reverts commit 60aa355b8e56a7b537f3a3e2e8ec7209ab93b794.
    
    The metaclass implementation of the singleton pattern doesn't preserve object
    identity upon unpickling, when there is already an instance of that singleton
    in the process. This shows up in the tests for `unpythonic.llist.nil`.
    
    Let's revert it for now, and think of a better strategy (maybe post 0.14.2).

--- unpythonic/test/test_misc.py
+++ unpythonic/test/test_misc.py
@@ -10,7 +10,7 @@ import threading
 
 from ..misc import call, callwith, raisef, pack, namelambda, timer, \
                    getattrrec, setattrrec, Popper, CountingIterator, ulp, slurp, \
-                   async_raise, Singleton
+                   async_raise
 from ..fun import withself
 
 def test():
@@ -237,17 +237,6 @@ def test():
         assert out[0] < 9  # terminated early due to the injected KeyboardInterrupt
     except NotImplementedError:
         print("async_raise not supported on this interpreter, ignoring test.")
-
-    # singleton object
-    class Foo(metaclass=Singleton):
-        pass
-    foo = Foo()
-    assert Foo() is foo
-
-    class Bar(Foo):
-        pass
-    bar = Bar()
-    assert Bar() is bar
 
     print("All tests PASSED")
 
