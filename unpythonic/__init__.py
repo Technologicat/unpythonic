4a0b9298a2d4357701736ddbc9c8c71f35263bce
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-08-07 16:41:57 +0300

    Update version number

--- unpythonic/__init__.py
+++ unpythonic/__init__.py
@@ -6,7 +6,7 @@ See ``dir(unpythonic)`` and submodule docstrings for more.
 If you have MacroPy installed, see also ``unpythonic.syntax``.
 """
 
-__version__ = '0.14.2'
+__version__ = '0.14.2.1'
 
 from .amb import *
 from .arity import *
