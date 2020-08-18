1c063a21a9caaa9dab248d8e65361b4fc16f2626
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-08-18 03:49:15 +0300

    WIP: some more macro tests converted to unpythonic.test.fixtures

--- unpythonic/syntax/test/test_forall.py
+++ unpythonic/syntax/test/test_forall.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 """Tuple comprehension with multiple-expression body."""
 
+from ...syntax import macros, test  # noqa: F401
+from ...test.fixtures import testset
+
 # There's also `deny`, which is `insist not`, but let's not import stuff we don't use.
-from ...syntax import macros, forall, insist  # noqa: F401
+from ...syntax import macros, forall, insist  # noqa: F401, F811
 
 def runtests():
-    # forall: pure AST transformation, with real lexical variables
-    #   - assignment (with List-monadic magic) is ``var << iterable``
-    out = forall[y << range(3),  # noqa: F821, `forall` defines the name on the LHS of the `<<`.
-                 x << range(3),  # noqa: F821
-                 insist(x % 2 == 0),  # noqa: F821
-                 (x, y)]  # noqa: F821
-    assert out == ((0, 0), (2, 0), (0, 1), (2, 1), (0, 2), (2, 2))
+    with testset("unpythonic.syntax.forall"):
+        # forall: pure AST transformation, with real lexical variables
+        #   - assignment (with List-monadic magic) is ``var << iterable``
+        out = forall[y << range(3),  # noqa: F821, `forall` defines the name on the LHS of the `<<`.
+                     x << range(3),  # noqa: F821
+                     insist(x % 2 == 0),  # noqa: F821
+                     (x, y)]  # noqa: F821
+        test[out == ((0, 0), (2, 0), (0, 1), (2, 1), (0, 2), (2, 2))]
 
-    # pythagorean triples
-    pt = forall[z << range(1, 21),   # hypotenuse  # noqa: F821
-                x << range(1, z + 1),  # shorter leg  # noqa: F821
-                y << range(x, z + 1),  # longer leg  # noqa: F821
-                insist(x * x + y * y == z * z),  # noqa: F821
-                (x, y, z)]  # noqa: F821
-    assert tuple(sorted(pt)) == ((3, 4, 5), (5, 12, 13), (6, 8, 10),
-                                 (8, 15, 17), (9, 12, 15), (12, 16, 20))
-
-    print("All tests PASSED")
+        # pythagorean triples
+        pt = forall[z << range(1, 21),   # hypotenuse  # noqa: F821
+                    x << range(1, z + 1),  # shorter leg  # noqa: F821
+                    y << range(x, z + 1),  # longer leg  # noqa: F821
+                    insist(x * x + y * y == z * z),  # noqa: F821
+                    (x, y, z)]  # noqa: F821
+        test[tuple(sorted(pt)) == ((3, 4, 5), (5, 12, 13), (6, 8, 10),
+                                   (8, 15, 17), (9, 12, 15), (12, 16, 20))]
 
 if __name__ == '__main__':
     runtests()
--- unpythonic/syntax/test/test_ifexprs.py
+++ unpythonic/syntax/test/test_ifexprs.py
@@ -1,35 +1,43 @@
 # -*- coding: utf-8 -*-
 """Extended if-expressions."""
 
-from ...syntax import macros, aif, it, cond, local  # noqa: F401
+from ...syntax import macros, test  # noqa: F401
+from ...test.fixtures import testset
+
+from ...syntax import macros, aif, it, cond, local  # noqa: F401, F811
 
 def runtests():
-    # Anaphoric if: aif[test, then, otherwise]
-    # Magic identifier "it" refers to the test result.
-    assert aif[2 * 21,
-               "it is {}".format(it),
-               "it is False"] == "it is 42"
+    with testset("unpythonic.syntax.ifexprs"):
+        with testset("aif (anaphoric if, you're `it`!)"):
+            # Anaphoric if: aif[test, then, otherwise]
+            # Magic identifier "it" refers to the test result.
+            test[aif[2 * 21,
+                     "it is {}".format(it),
+                     "it is False"] == "it is 42"]
 
-    # Lispy "cond" - a human-readable multi-branch conditional for lambdas.
-    answer = lambda x: cond[x == 2, "two",
-                            x == 3, "three",
-                            "something else"]
-    assert answer(42) == "something else"
+        with testset("cond (lispy multi-branch conditional expression)"):
+            answer = lambda x: cond[x == 2, "two",
+                                    x == 3, "three",
+                                    "something else"]
+            test[answer(2) == "two"]
+            test[answer(3) == "three"]
+            test[answer(42) == "something else"]
 
-    # implicit do: in any part of aif or cond, use extra brackets for a do[] environment
-    print("Testing aif/cond with implicit do")
-    assert aif[[local[x << 2 * 21], 2 * x],  # noqa: F821, the `local[]` macro defines the name on the LHS of the `<<`.
-               [print("hi"), "it is {}".format(it)],
-               [print("ho"), "it is False"]] == "it is 84"
+        # implicit do: in any part of aif or cond, use extra brackets for a do[] environment
+        with testset("integration with implicit do"):
+            print("Testing aif/cond with implicit do")
+            test[aif[[local[x << 2 * 21], 2 * x],  # noqa: F821, the `local[]` macro defines the name on the LHS of the `<<`.
+                     [print("hi"), "it is {}".format(it)],
+                     [print("ho"), "it is False"]] == "it is 84"]
 
-    # each "test" and "then" branch with multiple expressions should have its own
-    # extra brackets. (Similarly for the final "otherwise" branch.)
-    answer = lambda x: cond[[local[y << 2 * x], y == 4], [print("hi again"), "two"],  # noqa: F821
-                            x == 3, "three",
-                            "something else"]
-    assert answer(2) == "two"
-
-    print("All tests PASSED")
+            # each "test" and "then" branch with multiple expressions should have its own
+            # extra brackets. (Similarly for the final "otherwise" branch.)
+            answer = lambda x: cond[[local[y << 2 * x], y == 4], [print("hi again"), "two"],  # noqa: F821
+                                    x == 3, "three",
+                                    "something else"]
+            test[answer(2) == "two"]
+            test[answer(3) == "three"]
+            test[answer(4) == "something else"]
 
 if __name__ == '__main__':
     runtests()
