08037cb5176b8297774144a1f17f6fae00d8a805
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-08-19 02:31:59 +0300

    macro code: convert nb tests to unpythonic.test.fixtures
    This required changes to the block macro `nb`, which usually rewrites
    *all expressions* to save the value as `_`. The testing expr macros
    shouldn't count as expressions in that context.
    
    (The test macros don't even return a value; and they play a role
    similar to `assert`, which is a statement.)

--- unpythonic/syntax/nb.py
+++ unpythonic/syntax/nb.py
@@ -6,9 +6,11 @@ Auto-print top-level expressions, auto-assign last result as _.
 
 # This is the kind of thing thinking with macros does to your program. ;)
 
-from ast import Expr
+from ast import Expr, Subscript, Name, Call
 
 from macropy.core.quotes import macros, q, ast_literal  # noqa: F401
+
+from .util import isx
 
 def nb(body, args):
     p = args[0] if args else q[print]  # custom print function hook
@@ -18,7 +20,7 @@ def nb(body, args):
         theprint = ast_literal[p]
     newbody.extend(init)
     for stmt in body:
-        if type(stmt) is not Expr:
+        if type(stmt) is not Expr or istestmacro(stmt.value):
             newbody.append(stmt)
             continue
         with q as newstmts:
@@ -27,3 +29,22 @@ def nb(body, args):
                 theprint(_)
         newbody.extend(newstmts)
     return newbody
+
+# Integration with the test framework - ignore test[] et al. expressions.
+#
+# See unpythonic.syntax.test.testutil and unpythonic.test.fixtures.
+# TODO: move istestmacro to testutil?
+_test_macro_names = ["test", "test_signals", "test_raises", "error", "fail"]
+_test_function_names = ["unpythonic_assert",
+                        "unpythonic_assert_signals",
+                        "unpythonic_assert_raises"]
+def istestmacro(tree):
+    def isunexpandedtestmacro(tree):
+        return (type(tree) is Subscript and
+                type(tree.value) is Name and
+                tree.value.id in _test_macro_names)
+    def isexpandedtestmacro(tree):
+        return (type(tree) is Call and
+                any(isx(tree.func, fname, accept_attr=False)
+                    for fname in _test_function_names))
+    return isunexpandedtestmacro(tree) or isexpandedtestmacro(tree)
--- unpythonic/syntax/test/test_nb.py
+++ unpythonic/syntax/test/test_nb.py
@@ -1,29 +1,33 @@
 # -*- coding: utf-8 -*-
 
-from ...syntax import macros, nb  # noqa: F401
+from ...syntax import macros, test, test_raises, error  # noqa: F401
+from ...test.fixtures import testset
+
+from ...syntax import macros, nb  # noqa: F401, F811
 
 def runtests():
-    with nb:
-        assert _ is None  # noqa: F821, the `nb` macro defines `_` implicitly.
-        2 + 3          # top-level expressions autoprint, and auto-assign result to _
-        assert _ == 5  # ...and only expressions do that, so...  # noqa: F821
-        _ * 42         # ...here _ still has the value from the first line.  # noqa: F821
-        assert _ == 210  # noqa: F821
+    with testset("unpythonic.syntax.nb"):
+        with testset("basic usage"):
+            with nb:
+                test[_ is None]  # noqa: F821, the `nb` macro defines `_` implicitly.
+                2 + 3          # top-level expressions autoprint, and auto-assign result to _
+                test[_ == 5]  # ...and only expressions do that, so...  # noqa: F821
+                _ * 42         # ...here _ still has the value from the first line.  # noqa: F821
+                test[_ == 210]  # noqa: F821
 
-    try:
-        from sympy import symbols, pprint
-    except ImportError:
-        print("*** SymPy not installed, skipping symbolic math test ***")
-    else:
-        with nb(pprint):  # you can specify a custom print function (first positional arg)
-            assert _ is None  # noqa: F821
-            x, y = symbols("x, y")
-            x * y
-            assert _ == x * y  # noqa: F821
-            3 * _  # noqa: F821
-            assert _ == 3 * x * y  # noqa: F821
-
-    print("All tests PASSED")
+        with testset("integration with symbolic math"):
+            try:
+                from sympy import symbols, pprint
+            except ImportError:
+                error["SymPy not installed in this Python, cannot test symbolic math in nb."]
+            else:
+                with nb(pprint):  # you can specify a custom print function (first positional arg)
+                    test[_ is None]  # noqa: F821
+                    x, y = symbols("x, y")
+                    x * y
+                    test[_ == x * y]  # noqa: F821
+                    3 * _  # noqa: F821
+                    test[_ == 3 * x * y]  # noqa: F821
 
 if __name__ == '__main__':
     runtests()
