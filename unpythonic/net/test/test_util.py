488b09be724b1a2a92363652531178ebfc75613e
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-08-19 19:49:06 +0300

    convert unpythonic.net.test tests to unpythonic.test.fixtures
    Doesn't work yet due to a MacroPy issue with `bytes` literals,
    which are needed by these tests. See:
        https://github.com/azazel75/macropy/issues/26

--- unpythonic/net/test/test_util.py
+++ unpythonic/net/test/test_util.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8; -*-
 
+from ...syntax import macros, test  # noqa: F401
+from ...test.fixtures import testset
+
 from .fixtures import nettest
 
 from ..util import recvall, netstringify
 
 def runtests():
-    assert netstringify(b"hello world") == b"11:hello world,"
+    # TODO: As of MacroPy 1.1.0b2, this test module crashes at macro expansion time
+    # TODO: due to a MacroPy bug involving `bytes` literals.
+    # TODO: https://github.com/azazel75/macropy/issues/26
+    with testset("unpythonic.net.util"):
+        test[netstringify(b"hello world") == b"11:hello world,"]
 
-    server = lambda sock: recvall(1024, sock)
-    client = lambda sock: [sock.sendall(b"x" * 512), sock.sendall(b"x" * 512)]
-    assert len(nettest(server, client)) == 1024
-
-    print("All tests PASSED")
+        server = lambda sock: recvall(1024, sock)
+        client = lambda sock: [sock.sendall(b"x" * 512), sock.sendall(b"x" * 512)]
+        test[len(nettest(server, client)) == 1024]
 
 if __name__ == '__main__':
     runtests()
