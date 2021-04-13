65b372851ef8bc42f44c91bc5ec4a2e321976bc8
Juha Jeronen <juha.m.jeronen@gmail.com>
2021-04-14 02:13:41 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #
 """setuptools-based setup.py for unpythonic.
 
-Tested on Python 3.6.
+Tested on Python 3.8.
 
 Usage as usual with setuptools:
     python3 setup.py build
@@ -18,52 +18,46 @@ or
     python3 setup.py --help bdist_wheel  # or any command
 """
 
-#########################################################
-# General config
-#########################################################
-
-# Name of the top-level package of the library.
-#
-# This is also the top level of its source tree, relative to the top-level project directory setup.py resides in.
-#
-libname = "unpythonic"
-
-# Short description for package list on PyPI
-#
-SHORTDESC = "Supercharge your Python with parts of Lisp and Haskell."
-
-#########################################################
-# Init
-#########################################################
-
+import ast
 import os
-from setuptools import setup
+
+from setuptools import setup  # type: ignore[import]
+
 
 def read(*relpath, **kwargs):  # https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-setup-script
     with open(os.path.join(os.path.dirname(__file__), *relpath),
               encoding=kwargs.get('encoding', 'utf8')) as fh:
         return fh.read()
 
-# TODO: update version detector for Python 3.8 (accept also ast.Constant beside ast.Str)
-#
 # Extract __version__ from the package __init__.py
 # (since it's not a good idea to actually run __init__.py during the build process).
 #
 # http://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package
 #
-import ast
-init_py_path = os.path.join(libname, '__init__.py')
+init_py_path = os.path.join("unpythonic", "__init__.py")
 version = None
 try:
     with open(init_py_path) as f:
         for line in f:
-            if line.startswith('__version__'):
-                version = ast.parse(line).body[0].value.s
+            if line.startswith("__version__"):
+                module = ast.parse(line)
+                expr = module.body[0]
+                assert isinstance(expr, ast.Assign)
+                v = expr.value
+                if type(v) is ast.Constant:  # Python 3.8+
+                    # mypy understands `isinstance(..., ...)` but not `type(...) is ...`,
+                    # and we want to match on the exact type, not any subclass that might be
+                    # added in some future Python version.
+                    assert isinstance(v, ast.Constant)
+                    version = v.value
+                elif type(v) is ast.Str:
+                    assert isinstance(v, ast.Str)  # mypy
+                    version = v.s
                 break
 except FileNotFoundError:
     pass
 if not version:
-    raise RuntimeError("Version information not found in '{}'".format(init_py_path))
+    raise RuntimeError(f"Version information not found in {init_py_path}")
 
 #########################################################
 # Call setup()
@@ -72,25 +66,21 @@ if not version:
 setup(
     name="unpythonic",
     version=version,
+    packages=["unpythonic", "unpythonic.syntax"],
+    provides=["unpythonic"],
+    keywords=["functional-programming", "language-extension", "syntactic-macros",
+              "tail-call-optimization", "tco", "continuations", "currying", "lazy-evaluation",
+              "dynamic-variable", "macros", "lisp", "scheme", "racket", "haskell"],
+    install_requires=[],  # mcpyrate is optional for us, so we can't really put it here even though we recommend it.
+    python_requires=">=3.6,<3.10",
     author="Juha Jeronen",
     author_email="juha.m.jeronen@gmail.com",
     url="https://github.com/Technologicat/unpythonic",
-
-    # https://packaging.python.org/guides/making-a-pypi-friendly-readme/
-    description=SHORTDESC,
+    description="Supercharge your Python with parts of Lisp and Haskell.",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-
     license="BSD",
-
-    # free-form text field; http://stackoverflow.com/questions/34994130/what-platforms-argument-to-setup-in-setup-py-does
     platforms=["Linux"],
-
-    # See
-    #    https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    #
-    # for the standard classifiers.
-    #
     classifiers=["Development Status :: 4 - Beta",
                  "Environment :: Console",
                  "Intended Audience :: Developers",
@@ -98,39 +88,14 @@ setup(
                  "Operating System :: POSIX :: Linux",
                  "Programming Language :: Python",
                  "Programming Language :: Python :: 3",
-                 "Programming Language :: Python :: 3.4",
-                 "Programming Language :: Python :: 3.5",
                  "Programming Language :: Python :: 3.6",
                  "Programming Language :: Python :: 3.7",
+                 "Programming Language :: Python :: 3.8",
+                 "Programming Language :: Python :: 3.9",
                  "Programming Language :: Python :: Implementation :: CPython",
                  "Programming Language :: Python :: Implementation :: PyPy",
                  "Topic :: Software Development :: Libraries",
                  "Topic :: Software Development :: Libraries :: Python Modules"
                  ],
-
-    # See
-    #    http://setuptools.readthedocs.io/en/latest/setuptools.html
-    #
-    python_requires=">=3.4,<3.8",
-    install_requires=[],  # MacroPy is optional for us, so we can't really put "macropy3" here even though we recommend it.
-    # setup_requires=[],  # TODO: Using setup_requires is discouraged in favor of https://www.python.org/dev/peps/pep-0518/
-    # test_requires=["macropy3"],  # TODO: not very useful, because only "python3 setup.py test" installs these, and we don't use that.
-    provides=["unpythonic"],
-
-    # keywords for PyPI (in case you upload your project)
-    #
-    # e.g. the keywords your project uses as topics on GitHub, minus "python" (if there)
-    #
-    keywords=["functional-programming", "language-extension", "syntactic-macros",
-              "tail-call-optimization", "tco", "continuations", "currying", "lazy-evaluation",
-              "dynamic-variable", "macros", "lisp", "scheme", "racket", "haskell"],
-
-    # Declare packages so that  python -m setup build  will copy .py files (especially __init__.py).
-    #
-    # This **does not** automatically recurse into subpackages, so they must also be declared.
-    #
-    packages=["unpythonic", "unpythonic.syntax"],
-    scripts=["macropy3"],
-
-    zip_safe=False  # macros are not zip safe, because the zip importer fails to find sources, and MacroPy needs them.
+    zip_safe=False  # macros are not zip safe, because the zip importer fails to find sources.
 )
