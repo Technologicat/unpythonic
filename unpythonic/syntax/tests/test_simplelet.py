ce653adce591b8ff8e67e45ab2c0f62d054ae1dc
Juha Jeronen <juha.m.jeronen@gmail.com>
2021-04-28 03:18:15 +0300

    fix simplelet test
    Unlike `unpythonic`'s regular `let` constructs, the `simplelet` demo uses
    brackets, not parentheses, to define bindings. This matters when there is
    just one binding.

--- unpythonic/syntax/tests/test_simplelet.py
+++ unpythonic/syntax/tests/test_simplelet.py
@@ -8,13 +8,13 @@ from ...syntax.simplelet import macros, let, letseq  # noqa: F401, F811
 
 def runtests():
     with testset("let"):
-        test[let[(x, 21)][2 * x] == 42]  # noqa: F821, the `let` defines `x`.
-        test[let[(x, 21), (y, 2)][y * x] == 42]  # noqa: F821
+        test[let[[x, 21]][2 * x] == 42]  # noqa: F821, the `let` defines `x`.
+        test[let[[x, 21], [y, 2]][y * x] == 42]  # noqa: F821
 
     with testset("letseq"):
-        test[letseq[(x, 1)][2 * x] == 2]  # noqa: F821
-        test[letseq[(x, 1),  # noqa: F821
-                    (x, 2 * x)][2 * x] == 4]  # noqa: F821
+        test[letseq[[x, 1]][2 * x] == 2]  # noqa: F821
+        test[letseq[[x, 1],  # noqa: F821
+                    [x, 2 * x]][2 * x] == 4]  # noqa: F821
 
 if __name__ == '__main__':  # pragma: no cover
     with session(__file__):
