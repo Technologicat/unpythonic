f93339ec2fcd3c6ef1b063d6330cc908e926dba5
Juha Jeronen <juha.m.jeronen@gmail.com>
2021-05-01 19:09:56 +0300

    add dialects module docstring

--- unpythonic/dialects/__init__.py
+++ unpythonic/dialects/__init__.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
+"""Dialects: Python the way you want it.
+
+These dialects, i.e. whole-module syntax transformations, are powered by
+`mcpyrate`'s dialect subsystem. The user manual is at:
+    https://github.com/Technologicat/mcpyrate/blob/master/doc/dialects.md
+
+We provide these dialects mainly to demonstrate how to use that subsystem
+to customize Python beyond what a local macro expander can do.
+
+For examples of how to use the dialects, see the unit tests.
+"""
 
 # re-exports
 from .lispython import Lispython  # noqa: F401
