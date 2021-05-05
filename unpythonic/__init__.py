2314b207bfe0cc32360783c485facd9b718fcfc7
Juha Jeronen <juha.m.jeronen@gmail.com>
2021-05-05 13:35:34 +0300

    bump version

--- unpythonic/__init__.py
+++ unpythonic/__init__.py
@@ -7,7 +7,7 @@ If you have ``mcpyrate`` installed, see also ``unpythonic.syntax``
 for a trip down the rabbit hole.
 """
 
-__version__ = '0.14.3'
+__version__ = '0.15.0'
 
 from .amb import *  # noqa: F401, F403
 from .arity import *  # noqa: F401, F403
