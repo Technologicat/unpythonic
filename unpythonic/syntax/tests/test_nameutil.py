0ae54a194aeae0445114d4e9aca818948dbec3a0
Juha Jeronen <juha.m.jeronen@gmail.com>
2021-05-05 14:31:16 +0300

    add some missing unit tests

--- unpythonic/syntax/tests/test_nameutil.py
+++ unpythonic/syntax/tests/test_nameutil.py
@@ -6,7 +6,10 @@ from ...test.fixtures import session, testset
 
 from mcpyrate.quotes import macros, q, h  # noqa: F401, F811
 
-from ...syntax.nameutil import isx, getname
+from mcpyrate.expander import MacroExpander
+
+from ...syntax.nameutil import (isx, getname,
+                                is_unexpanded_expr_macro, is_unexpanded_block_macro)
 
 from ast import Call
 
@@ -31,6 +34,31 @@ def runtests():
         test[getname(captured.func) == "capture_this"]
         test[getname(attribute) == "ok"]
         test[getname(attribute, accept_attr=False) is None]
+
+    with testset("is_unexpanded_expr_macro"):
+        def dummymacro(tree, **kw):
+            return tree
+        m = MacroExpander({"dummy": dummymacro}, filename="<fake filename for tests in test_nameutil.py>")
+
+        # we need a macro that is bound in the expander we pass to the analyzer.
+        test[is_unexpanded_expr_macro(dummymacro, m, q[dummy[blah]])]  # noqa: F821, only quoted
+        test[not is_unexpanded_expr_macro(dummymacro, m, q[notdummy[blah]])]  # noqa: F821, only quoted
+        test[not is_unexpanded_expr_macro(dummymacro, m, q[42])]
+
+    with testset("is_unexpanded_block_macro"):
+        with q as quoted:
+            with dummy:  # noqa: F821, only quoted
+                ...
+        test[is_unexpanded_block_macro(dummymacro, m, quoted[0])]
+
+        with q as quoted:
+            with notdummy:  # noqa: F821, only quoted
+                ...
+        test[not is_unexpanded_block_macro(dummymacro, m, quoted[0])]
+
+        with q as quoted:
+            a = 42  # noqa: F841
+        test[not is_unexpanded_block_macro(dummymacro, m, quoted[0])]
 
 if __name__ == '__main__':  # pragma: no cover
     with session(__file__):
